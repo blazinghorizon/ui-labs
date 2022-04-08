d8f6387b91de884033633eed99f2b354d10bdee6
blazinghorizon <apexkennys@gmail.com>
2022-04-08 12:11:26 +0300

    fixed 1.3

--- lab1/1.3/final.py
+++ lab1/1.3/final.py
@@ -1,10 +1,12 @@
+from re import L
 from tkinter import *
 from tkinter import ttk
 import locale
+import re
 
-from numpy import take
+cur_locale = locale.getlocale()
 
-print(locale.getlocale())
+locale.setlocale(locale.LC_NUMERIC, cur_locale)
 
 root = Tk()
 root.title("UI 1.3")
@@ -13,9 +15,40 @@ root.title("UI 1.3")
 value = StringVar()
 farenheit = StringVar()
 celcius = StringVar()
+error_msg = StringVar()
+
+def output_error_msg():
+    pass
+
+def parse_to_float(value):
+    if value == "-":
+        return value
+
+    c = (locale.delocalize(value))
+    try:
+        float_c = float(c)
+        error_msg.set("")
+    except:
+        error_msg.set("Введеные данные некорректны!")
+        print('exception')
+        return
+
+    print(float(c))
+
+    #print(locale.format_string("%f", value))
+    #locale.delocalize()
+    
+
+def check_input(value):
+    if len(value) == 0:
+        return
+    else:
+        parsed = parse_to_float(value)
+                
 
 def calculate(var, index, mode):
-    print(value.get())
+    if not check_input(value.get()):
+        return
     farenheit.set(value.get())
     celcius.set(value.get())
     pass
@@ -23,14 +56,14 @@ def calculate(var, index, mode):
 value.trace("w", callback=calculate)
 
 # create a subwindow inside the main window with paddings
-content = ttk.Frame(root, padding=(3,3,12,12))
+content = ttk.Frame(root, padding=(3,3,8,12))
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 
 # create labels for text
-value_label=ttk.Label(content, text="Введите значение:")
-farenheit_label = ttk.Label(content, text="Из Фаренгейта в Цельсия:")
-celcius_label = ttk.Label(content, text="Из Цельсия в Фаренгейт:")
-
+farenheit_label = ttk.Label(content, text="Из F в C:", anchor='center')
+celcius_label = ttk.Label(content, text="Из C в F:", anchor='center')
+error_label = ttk.Label(content, textvariable=error_msg)
+value_label=ttk.Label(content, text="Значение:", anchor='center')
 # 0-1 row <center>Label + Entry<center>
 
 # 2 row <left>Label + Label<left> <right>Label+Label<right>
@@ -38,8 +71,9 @@ celcius_label = ttk.Label(content, text="Из Цельсия в Фаренгей
 
 # place labels on the grid
 value_label.grid(row=0, column=0, sticky="news")
-farenheit_label.grid(row=1, column=0, sticky="news")
-celcius_label.grid(row=2, column=0, sticky="news")
+error_label.grid(row=1, column=0, columnspan=2, sticky="news")
+farenheit_label.grid(row=2, column=0, sticky="news")
+celcius_label.grid(row=3, column=0, sticky="news")
 
 value_entry = ttk.Entry(content, textvariable=value, takefocus=1)
 farenheit_entry = ttk.Entry(content, textvariable=farenheit, takefocus=0)
@@ -49,9 +83,9 @@ celcius_entry = ttk.Entry(content, textvariable=celcius, takefocus=0)
 
 # place entries on the grid
 value_entry.grid(row=0,column=1, padx=3, pady=3, sticky="ew")
-farenheit_entry.grid(row=1,column=1, padx=3, pady=3, sticky="ew")
+farenheit_entry.grid(row=2,column=1, padx=3, pady=3, sticky="ew")
 #farenheit_dependency.grid(row=3,column=1, padx=3, pady=3, sticky="ew")
-celcius_entry.grid(row=2,column=1, padx=3, pady=3, sticky="ew")
+celcius_entry.grid(row=3,column=1, padx=3, pady=3, sticky="ew")
 #celcius_dependency.grid(row=3,column=2, padx=3, pady=3, sticky="ew")
 
 # configure root and content for dynamic resizing
@@ -63,6 +97,7 @@ content.columnconfigure(1, weight=1)
 content.rowconfigure(0, weight=1)
 content.rowconfigure(1, weight=1)
 content.rowconfigure(2, weight=1)
+content.rowconfigure(3, weight=1)
 
 # set minsize for window
 root.update()
---
+++ lab1/1.3/test.py
@@ -0,0 +1,16 @@
+import locale
+
+cur_locale = locale.getlocale()
+
+#locale.setlocale(locale.LC_NUMERIC, "es_ES")
+locale.setlocale(locale.LC_NUMERIC, cur_locale)
+
+num1 = "1,5"
+num2 = "1.500,2"
+num3 = "-1,5"
+num4 = "-1.5"
+
+print(locale.delocalize(num1))
+print(locale.delocalize(num2))
+print(locale.delocalize(num3))
+print(locale.delocalize(num4))
